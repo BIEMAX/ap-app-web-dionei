c4abe9a80cedf44a3a22be4c57f890997017bd35
BIEMAX <tecnico.beilke@gmail.com>
2022-08-04 10:34:58 -0300

    Small changes and fixed wrong text in mobile view

--- src/app/about-me/about-me.component.ts
+++ src/app/about-me/about-me.component.ts
@@ -23,19 +23,24 @@ export class AboutMeComponent implements OnInit {
         'This is me:', 
         true, 
         'https://media-exp1.licdn.com/dms/image/C4E03AQHNrvo7buReoQ/profile-displayphoto-shrink_800_800/0/1628681073467?e=1665014400&v=beta&t=enOf2MnXr2wejHDl9UqXM3WY2x3mWqY6lv-uj0tHMHk', 
-        `Um cara determinado, que começou muito novo na área de TI 
-        e hoje é completamente apaixonado pelo o que faz.
+        `Um cara determinado, que começou muito novo
+        na área de TI e hoje é completamente 
+        apaixonado pelo o que faz.
         
-        Tenho 27 anos, sou casado com uma linda mulher, 
-        atualmente sem filhos e sem animais de estimação.
+        Tenho 27 anos, sou casado com uma linda 
+        mulher, atualmente sem filhos e 
+        sem animais de estimação.
 
-        Um apaixonado por literatura, Marvel, DC Comics,
-        senhor dos anéis, jogos de mais variados tipos.
+        Um apaixonado por literatura, Marvel, 
+        DC Comics, senhor dos anéis, jogos 
+        de mais variados tipos, cozinheiro
+        por natureza e admirador da física
+        quântica.
 
-        Sou um nerd desde sempre, e sou completamente
-        fascinado pela física aplicada, quântica, 
-        matemática e astronomia (estudo dos astros).
-        
+        Sou um nerd desde sempre, e sou 
+        completamente fascinado pela física
+        aplicada, quântica, matemática e 
+        astronomia (estudo dos astros).
         `,
         false, false
       ),
